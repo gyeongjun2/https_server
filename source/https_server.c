3e5ee7536b70bf43b2f59d3419d245a0fc75cd80
jaeyu5034 <pyyp5034@gmail.com>
2024-12-17 19:58:41 +0900

    Update ca

--- source/https_server.c
+++ source/https_server.c
@@ -29,17 +29,17 @@ int main(int argc, char *argv[]) {
     const SSL_METHOD *method = TLS_server_method();
     SSL_CTX *ctx = SSL_CTX_new(method);
 
-    if (!ctx) {
+    if(!ctx){
         ERR_print_errors_fp(stderr);
         exit(1);
     }
 
-    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+    if(SSL_CTX_use_certificate_file(ctx, "/etc/letsencrypt/live/pgj-server.n-e.kr/fullchain.pem", SSL_FILETYPE_PEM) <= 0){
         ERR_print_errors_fp(stderr);
         exit(2);
     }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0) {
+    if(SSL_CTX_use_PrivateKey_file(ctx, "/etc/letsencrypt/live/pgj-server.n-e.kr/privkey.pem", SSL_FILETYPE_PEM) <= 0){
         ERR_print_errors_fp(stderr);
         exit(3);
     }
