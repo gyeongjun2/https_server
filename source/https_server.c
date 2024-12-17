90359b3ef34c293965268ac556715ed376c9f44f
jaeyu5034 <pyyp5034@gmail.com>
2024-12-17 18:00:15 +0900

    Update PORT

--- source/https_server.c
+++ source/https_server.c
@@ -9,6 +9,7 @@
 #include <openssl/err.h>
 
 #define SIZE 1024
+#define PORT 8000
 
 void *handle_client_thread(void *arg);
 void error_handling(char *message);
@@ -57,7 +58,7 @@ int main(int argc, char *argv[]) {
     memset(&serv_addr, 0x00, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(atoi(argv[1]));
+    serv_addr.sin_port = htons(PORT);
 
     if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
         error_handling("bind() error");
